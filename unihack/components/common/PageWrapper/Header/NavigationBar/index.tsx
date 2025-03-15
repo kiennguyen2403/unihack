ad637528035aff583ad40862814f404a09850d62
kevicebryan <kevinbryanreligion@gmail.com>
2025-03-15 12:21:33 +1100

    remove: unused

--- unihack/components/common/PageWrapper/Header/NavigationBar/index.tsx
+++ unihack/components/common/PageWrapper/Header/NavigationBar/index.tsx
@@ -14,17 +14,8 @@ const NavigationBar = () => {
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
       <div className="px-8 w-full flex gap-5 items-center justify-between font-semibold">
         <Link href={"/"}>Thubble.</Link>
-        {/* TODO: add logo here */}
-        <div className="flex items-center justify-center gap-6 font-light">
-          {/* TODO: add list of pages here */}
-          {/* <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About</Link>
-          <Link href={"/contact"}>Contact</Link> */}
-        </div>
+
         <div className="flex items-center gap-4">
-          {/* TODO: add login and register buttons here */}
-          {/* <Button variant="outline">Login</Button>
-          <Button>Sign up</Button> */}
           <ThemeSwitcher />
           <SignedOut>
             <SignInButton />
